5bd999e1725e421a1db4863b8efcf0c9a9f6d72f
ProfitWarning <christian.fastner@arcor.de>
2017-03-10 19:27:40 +0100

    final

--- day1/after/src/app/app.component.ts
+++ day1/after/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Dish} from './dish';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  dish: Dish = {
+    id: 1,
+    name: 'Pizza Margherita',
+    price: 8
+  };
 }
